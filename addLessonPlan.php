05f01329a605c08cd50dac798003a9b813264c68
unknown <qdevorah01499@gmail.com>
2018-07-04 05:58:20 +0800

    Devorah added initial addLessonPlan.php file

--- addLessonPlan.php
+++ addLessonPlan.php
@@ -1,9 +1,11 @@
 <?php
 
 	include("sql_connect.php");
-
+	
+	$venue = mysqli_real_escape_string($sql, $_POST['venue']);
 	$datePlan = mysqli_real_escape_string($sql, $_POST['datePlan']);
-  	$venue = mysqli_real_escape_string($sql, $_POST['venue']);
+  	$date_created = date("mm/dd/yyyy");
+  	$date_updated = date("mm/dd/yyyy");
   	$lessonOutline = mysqli_real_escape_string($sql, $_POST['lessonOutline']);
   	$objectives = mysqli_real_escape_string($sql, $_POST['objectives']);
   	$instructions = mysqli_real_escape_string($sql, $_POST['instructions']);
@@ -13,7 +15,7 @@
 
 
 
-  	$result = $sql->query("INSERT INTO lesson_plan(venue, date_created, date_of_plan, date_updated, lesson_outline, objectives, instruction, motivation, materials) VALUES('$venue', '$employee_type', '$employee_dob', '$shift_time','$lessonOutline', '$objectives', '$instruction', '$motivation', '$materials')");
+  	$result = $sql->query("INSERT INTO lesson_plan(venue, date_created, date_of_plan, date_updated, lesson_outline, objectives, instruction, motivation, materials) VALUES('$venue', '$date_created', 'datePlan', '$date_updated','$lessonOutline', '$objectives', '$instruction', '$motivation', '$materials')");
 
 
 ?>
