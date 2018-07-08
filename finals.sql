756eb5113287be8e059b9a252e38bdbfae8e96f2
Brandon Benedict <brandonbenedict.biano@gmail.com>
2018-07-08 13:33:59 +0800

    Edited the activity table

--- finals.sql
+++ finals.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 03, 2018 at 01:45 PM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.2
+-- Generation Time: Jul 08, 2018 at 07:26 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,8 +32,17 @@ CREATE TABLE `activity` (
   `activity_id` int(11) NOT NULL,
   `plan_id` int(11) NOT NULL,
   `grade` float(3,2) NOT NULL,
-  `grade_id` int(11) NOT NULL
+  `student_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `activity`
+--
+
+INSERT INTO `activity` (`activity_id`, `plan_id`, `grade`, `student_id`) VALUES
+(1, 1, 9.99, 15100106),
+(2, 1, 0.00, 15100107),
+(3, 1, 1.50, 15100108);
 
 -- --------------------------------------------------------
 
@@ -67,6 +76,18 @@ CREATE TABLE `grade` (
 -- --------------------------------------------------------
 
 --
+-- Stand-in structure for view `lessonview`
+-- (See below for the actual view)
+--
+CREATE TABLE `lessonview` (
+`date_of_plan` date
+,`venue` varchar(50)
+,`user_id` int(11)
+);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `lesson_plan`
 --
 
@@ -96,7 +117,7 @@ INSERT INTO `lesson_plan` (`plan_id`, `subject_id`, `schedule_id`, `venue`, `tea
 (1, 3, 1, 'Gym', 15100103, '2018-06-09', '2018-06-11', '2018-06-10', 'The students can determine the importance of physical fitness and can also explain the different components of physical fitness.', 'Students can explain the aspects of physical fitness, list and define factors that impact physical fitness, and create physical fitness personal plan.  ', 'Engage the students with topic by asking them what the term ‘Physical fitness’ is to them. Show a video clip about physical fitness. Then divide the class into 7 groups and give each group a marker. Let them write each physical fitness components and explain. Let them report their work in front of the class after 45 minutes. After each report they will prepare ¼ piece of paper and write their learnings about the lesson for today.', 'The group who receives the highest grade will be given plus points and a chocolate bar.', 'Manila Paper, Marker', 15100105, 1),
 (2, 2, 2, 'Quadrangle', 15100102, '2018-06-10', '2018-06-11', '2018-06-10', 'This visual arts lesson will allow students to get creative while practicing with either photography or videography. Students will also have to consider perspective from things outside themselves. Rubric included', 'The learners will create a photo array with narrative or a short video with sound sharing the perspective of an inanimate object. Students will be expected to be creative in both the verbal and artistic areas of the assignment.', 'Students will choose an inanimate object in the building or at their home to photograph or use for the video project.\r\nThe first shot will be of the actual object, but all other photos or video shots will be from the perspective of the object.\r\nAdditionally, commentary will be offered about how the object may think about the world.\r\n', 'Ask students to think about the world from that perspective for a moment.\r\nNow have students think about the world if they could only see from that position they are in, all day, every day, in the same spot.\r\nStudents with the best shot will be exhibited in the mural.\r\n', 'Cameras or video cameras for each student\r\nComputers/printers for completion of the project (any appropriate video sharing platform or photo sharing program should work)\r\n', 15100105, 1),
 (3, 1, 3, 'Music Room 2', 15100101, '2018-06-09', '2018-06-12', '2018-06-10', 'The students will learn how to read musical notes.', 'The learners will be able to know the names of each note and their corresponding measure.\r\nLecture will be followed by a test to measure student’s understanding about the lesson\r\n', 'Students will listen to a number of notes at a time with the aid of a metronome to help them to be familiar with the measures of notes. \r\nThe learners will be asked few questions related to the topic discussed.\r\n', 'The student/s who got the highest score will be asked about their favorite music as of the moment and it’ll be the piece the class will learn and play at the end of the 1st quarter.', 'Materials Needed\r\nPapers, pens, metronome, stereo, audio file.\r\n', 15100105, 1),
-(4, 4, 4, 'Library and Classroom', 15100104, '2018-06-10', '2018-06-12', '2018-06-10', 'Students will lean the long history of embroidery and the different symbolisms of embroidery.', 'Students will be able to explain the history and symbolisms of embroidery.', 'The students will be group into 7. Each group will be assigned to a timeline and will research about its history in embroidery. They will then present their research in front of the class through role play. Each group will be given 5 minutes to present their role play and explain their role play. ', 'The group who scores the highest will receive a perfect score and will receive a bonus point.', 'Music Play', 15100105, 1),
+(4, 4, 4, 'Library and Classroom', 15100104, '2018-06-10', '2018-06-12', '2018-06-10', 'Students will lean the long history of embroidery and the different symbolisms of embroidery.', 'Students will be able to explain the history and symbolisms of embroidery.', 'The students will be group into 7. Each group will be assigned to a timeline and will research about its history in embroidery. They will then present their research in front of the class through role play. Each group will be given 5 minutes to present their role play and explain their role play. ', 'The group who scores the highest will receive a perfect score and will receive a bonus point.', 'Music Player', 15100105, 1),
 (5, 3, 1, 'Gym', 15100103, '2018-06-09', '2018-06-13', '2018-06-10', 'This lesson will allow students to determine different physical fitness test exercise and perform each test with the materials provided.', 'Students will able to learn about the different physical fitness test exercise. Performing the test exercise can help keep student’s body at a healthy weight. This can also decrease a person’s risk of developing certain diseases like obesity, type 2 diabetes, and high blood pressure.', 'Divide the class into 7 groups, each member of the group must explain the different types of physical fitness test that they could remember. They will be given 20-30 minutes for this activity. After 30 minutes, they will report their ideas in front of the class. After each report of the group, the teacher will give a quick explanation regarding each test. After the report, Students will do a short exercise before performing the different types of physical fitness test provided by the teacher. The materials are scattered around the gym area in order for the students to freely perform the exercises. They will perform this activity for the entire encounter', 'Students with the highest number of physical fitness test that they could remember will be given plus points and a chocolate bar. ', 'Chalk, measuring stick, small traffic cones, weight balance, tape measure, ', 15100105, 1),
 (6, 2, 2, 'Quadrangle', 15100102, '2018-06-10', '2018-06-13', '2018-06-10', 'This lesson will allow students to practice creating art from a unique perspective while learning about Michelangelo.', 'Students should be able to create a unique drawing from a different perspective, just as when Michelangelo painted the Sistine Chapel ceiling.', 'Have students tape a piece of paper under their desks or tables.\r\nHave all art supplies at arm’s reach.\r\nAllow students to create a unique piece of art, but explain that the whole paper should be covered.\r\n', 'Ask the students if they have heard about Michelangelo or the Sistine Chapel.\r\nExplain that today they will try to create something beautiful while painting like Michelangelo.\r\nStudents with the best painting will be exhibited outside the classroom for one week.\r\n', 'Paper, paints (for older students), crayons/markers (for younger students), tape, floor covering where needed', 15100105, 1),
 (7, 1, 3, 'Music Room 2', 15100101, '2018-06-10', '2018-06-14', '2018-06-10', 'This lesson will allow students to make music sheets.', 'Students will learn which part of the sheet a note should be placed based on the given letter in the music alphabet/note.', 'Students will be flashed a series of letters and will place a whole note on the sheet music basing from the letter given.', 'Student who got the highest remark will be able to choose his/her own piece to play at the end of the 1st quarter with an additional 10 points to his/her 1st quarter exam grade.', 'Paper, pen.', 15100105, 1),
@@ -186,7 +207,9 @@ INSERT INTO `subject` (`subject_id`, `course_code`, `course_name`) VALUES
 (1, 'Musc1', 'Music 1'),
 (2, 'Arts11', 'Arts 11'),
 (3, 'PE11', 'Physical Education 11'),
-(4, 'HE11', 'Home Economics 11');
+(4, 'HE11', 'Home Economics 11'),
+(8, 'bech ka', 'This is about how  to obey gail'),
+(9, 'PROSTI 101', 'Kid in the streets, beast in the sheets.');
 
 -- --------------------------------------------------------
 
@@ -260,6 +283,15 @@ INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `type`, `filename`, `tow
 (15100124, 'Margeret', 'Lugo', 'student', '', 'Talamban', 'Cebu', 'Philippines', 6000, '', ''),
 (15100125, 'Lara', 'Ahmad', 'student', '', 'Talamban', 'Cebu', 'Philippines', 6000, '', '');
 
+-- --------------------------------------------------------
+
+--
+-- Structure for view `lessonview`
+--
+DROP TABLE IF EXISTS `lessonview`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `lessonview`  AS  select `lesson_plan`.`date_of_plan` AS `date_of_plan`,`lesson_plan`.`venue` AS `venue`,`user`.`user_id` AS `user_id` from (((`lesson_plan` join `subject` on((`lesson_plan`.`subject_id` = `subject`.`subject_id`))) join `teacher` on((`subject`.`subject_id` = `teacher`.`subject_id`))) join `user` on((`teacher`.`teacher_id` = `user`.`user_id`))) ;
+
 --
 -- Indexes for dumped tables
 --
@@ -269,7 +301,8 @@ INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `type`, `filename`, `tow
 --
 ALTER TABLE `activity`
   ADD PRIMARY KEY (`activity_id`),
-  ADD KEY `plan_id` (`plan_id`);
+  ADD KEY `plan_id` (`plan_id`),
+  ADD KEY `student_id` (`student_id`);
 
 --
 -- Indexes for table `coordinator`
@@ -335,7 +368,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `activity`
 --
 ALTER TABLE `activity`
-  MODIFY `activity_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `activity_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `lesson_plan`
@@ -353,7 +386,7 @@ ALTER TABLE `schedule`
 -- AUTO_INCREMENT for table `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -369,7 +402,8 @@ ALTER TABLE `user`
 -- Constraints for table `activity`
 --
 ALTER TABLE `activity`
-  ADD CONSTRAINT `activity_ibfk_1` FOREIGN KEY (`plan_id`) REFERENCES `lesson_plan` (`plan_id`);
+  ADD CONSTRAINT `activity_ibfk_1` FOREIGN KEY (`plan_id`) REFERENCES `lesson_plan` (`plan_id`),
+  ADD CONSTRAINT `activity_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`);
 
 --
 -- Constraints for table `coordinator`
